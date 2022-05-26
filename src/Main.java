8b0b88f4be4270a6377b940c365b4765fca14f06
Elena Kutsenko <albion.k@mail.ru>
2022-05-26 17:53:41 +0300

    правка

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String firstName = "Ivan";
+        String middleName = " Ivanovich";
+        String lastName = " Ivanov";
+        String fullName = firstName + middleName + lastName;
+        {
+            System.out.println("ФИО сотрудника — " + fullName);
+        }
+        String fullName2 = fullName.toUpperCase();
+        {
+            System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName2);
+        }
+
+        String fullName3 = fullName.replace(" ", ";");
+        {
+            System.out.println("Данные ФИО сотрудника для административного отдела — " + fullName3);
+        }
+        {
+
+        fullName = "Иванов Семён Семёнович";
+        String replace = fullName.replace("ё", "e");
+        System.out.println(replace);
+
+        }
     }
 }
